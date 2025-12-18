54088563ab6813ce74aad0e8dc6f9ff92f52a03b
singh-imanshu <himanshu.20245044@mnnit.ac.in>
2025-12-18 19:58:27 +0530

    upgrade springdoc and improve error handling

--- src/main/java/com/himanshu/resume_ai_analyzer/exception/GlobalExceptionHandler.java
+++ src/main/java/com/himanshu/resume_ai_analyzer/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 /**
  * Global exception handler to capture and format errors across the application.
@@ -12,6 +13,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ProblemDetail handleNotFound(NoResourceFoundException e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleUnhandledException(Exception e) {
         ProblemDetail problem = ProblemDetail.forStatusAndDetail(
